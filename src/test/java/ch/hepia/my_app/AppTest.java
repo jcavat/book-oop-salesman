8d6617ffe84678af268618550a960e22f63f3449
Joel Cavat <jcavat@gmail.com>
2020-06-19 08:34:32 +0200

    Add some basic tests

--- src/test/java/ch/hepia/my_app/AppTest.java
+++ src/test/java/ch/hepia/my_app/AppTest.java
@@ -1,18 +1,29 @@
 package ch.hepia.my_app;
 
+import ch.hepia.my_app.geometry.Point;
+import ch.hepia.my_app.optimization.NearestNeighbourProblem;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- * Unit test for simple App.
- */
 class AppTest {
 
+    final Point p1 = new Point(0.0, 0.0);
+    final Point p2 = new Point(3.0, 4.0);
+
     @Test
-    void dummyTest() {
-        assertTrue(true);
+    void testRoundTripBetweenTwoPoints() {
+        NearestNeighbourProblem problem = new NearestNeighbourProblem();
+        problem.addPoints(p1, p2);
+        assertEquals(p1.distanceWith(p2) * 2, problem.solve().totalDistance());
     }
+
+    @Test
+    void testSolutionWithoutPoints() {
+        NearestNeighbourProblem problem = new NearestNeighbourProblem();
+
+        /* should throw an error. Second arg means "execute it later" */
+        assertThrows(IllegalStateException.class, () -> problem.solve());
+    }
+
 }
