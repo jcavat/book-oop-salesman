c7e33b21b275d41c5d57eb1dd34899c6dc8235a6
Joel Cavat <jcavat@gmail.com>
2020-06-30 11:45:48 +0200

    Remove public for tests

--- src/test/java/ch/hepia/my_app/optimization/NearestNeighbourProblemTest.java
+++ src/test/java/ch/hepia/my_app/optimization/NearestNeighbourProblemTest.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class NearestNeighbourProblemTest {
+class NearestNeighbourProblemTest {
 
     @Test
     void testSolutionWithoutPoints() {
